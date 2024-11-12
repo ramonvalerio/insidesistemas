c05a6ca34c30410ccc81c7f416a6714da4dfa95f
Ramon Valerio da Silva <ramonvalerios@gmail.com>
2024-11-12 15:59:04 -0300

    add PedidoController

---
+++ InsideSistemas.Api/Controllers/PedidoController.cs
@@ -0,0 +1,64 @@
+﻿using InsideSistemas.Application.DTOs;
+using InsideSistemas.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsideSistemas.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IPedidoAppService _pedidoAppService;
+
+        public PedidoController(IPedidoAppService pedidoAppService)
+        {
+            _pedidoAppService = pedidoAppService;
+        }
+
+        [HttpGet("{pedidoId}")]
+        public async Task<IActionResult> ObterPedidoPorId(int pedidoId)
+        {
+            var pedido = await _pedidoAppService.ObterPedidoPorIdAsync(pedidoId);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            return Ok(pedido);
+        }
+
+        [HttpGet("listar")]
+        public async Task<IActionResult> ListarPedidos()
+        {
+            var pedidos = await _pedidoAppService.ListarPedidosAsync();
+            return Ok(pedidos);
+        }
+
+        [HttpPost("iniciar")]
+        public async Task<IActionResult> IniciarNovoPedido()
+        {
+            var pedido = await _pedidoAppService.IniciarNovoPedidoAsync();
+            return Ok(pedido);
+        }
+
+        [HttpPost("{pedidoId}/adicionar-produto")]
+        public async Task<IActionResult> AdicionarProdutoAoPedido(int pedidoId, [FromBody] ProdutoDTO produto)
+        {
+            var pedidoAtualizado = await _pedidoAppService.AdicionarProdutoAoPedidoAsync(pedidoId, produto);
+            return Ok(pedidoAtualizado);
+        }
+
+        [HttpDelete("{pedidoId}/remover-produto/{produtoId}")]
+        public async Task<IActionResult> RemoverProdutoDoPedido(int pedidoId, int produtoId)
+        {
+            var pedidoAtualizado = await _pedidoAppService.RemoverProdutoDoPedidoAsync(pedidoId, produtoId);
+            return Ok(pedidoAtualizado);
+        }
+
+        [HttpPost("{pedidoId}/fechar")]
+        public async Task<IActionResult> FecharPedido(int pedidoId)
+        {
+            var pedidoFechado = await _pedidoAppService.FecharPedidoAsync(pedidoId);
+            return Ok(pedidoFechado);
+        }   
+    }
+}
